f62ab0a0d00cc45fabc7964ef4ed9c4cc73b8b0e
mustafa3306 <mustafaworkkok@gmail.com>
2022-06-29 13:55:07 +0300

    js finished

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,18 +1,28 @@
+import { useState } from "react"
 import "./Card.css"
 
 const Card = ({ name, img, options }) => {
+    const [isClick, setIsClick] = useState(true);
 
     return (
         <div className="container">
-            <div className="first">
-               <img src={img} alt="" />
-               <h1>{name}</h1>
-            </div>
-            <div className="second">
-            {options.map((items)=>{
-                return <li>{items}</li>
-            })}
-            </div>
+            {
+                isClick
+                ?
+                <div onClick={() => setIsClick(false)} className="first">
+                <img src={img} alt="" />
+                <h1>{name}</h1>
+             </div>
+             :
+             <div onClick={()=> setIsClick(true)} className="second">
+             {options.map((items)=>{
+                 return <li>{items}</li>
+             })}
+             </div>
+            }
+            
+            
+
         </div>
     )
 }
--- src/components/card/Main.jsx
+++ src/components/card/Main.jsx
@@ -5,7 +5,7 @@ import "./Main.css"
 const Main = () => {
     return (
         <div className="main">
-         <h1>Languages</h1>   
+         <h1 className="title">Languages</h1>   
         <div className="cards">
             {languages.map((item, index) => <Card {...item} key={index} />)}
         </div>
